3d06f744eed8319fc594f210c0c8b5768acbaaef
John Smith <example.stanford.edu>
2013-07-23 07:43:14 +0000

    added

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var buf = new buffer(fs.readFile('index.html'));
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  /*response.send('Hello World 2!');*/
+   response.send(buf.toString());   
 });
 
 var port = process.env.PORT || 5000;
